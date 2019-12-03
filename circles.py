fc4494add36c07f5b7454cc35f35ce7fe4aa5907
hingobway <hingobway@users.noreply.github.com>
2019-12-03 14:39:08 -0500

    first bg replacement, switched to orange tracker

--- circles.py
+++ circles.py
@@ -1,6 +1,21 @@
+import cv2 as cv
+
+
 class Color:
     def __init__(self, color):
         self.color = color
+
+    def draw(self, img, x, y, radius):
+        cv.circle(img, (x, y), radius, self.color, -1)
+
+
+# class Image:
+#     def __init__(self):
+#         #
+
+#     def draw(self, img, x, y, radius):
+
+#         #
 
 
 circles = [
--- main.py
+++ main.py
@@ -23,16 +23,27 @@ lines = []
 draw = False
 newLine = False
 
-color=(255,255,255)
+color = (255, 255, 255)
 pad = 15
+curBG=0
+
+frame=None
 
 while(True):
     _, img = cap.read()
+    orig=img
     # img = cv.resize(img, (1920,1010), interpolation=cv.INTER_LINEAR)
-    height,width=img.shape[:2]
-
+    height, width = img.shape[:2]
 
     img = cv.flip(img, 1)
+    
+    try:
+        frame=cv.imread('assets/cur.jpg')
+    except:
+        pass
+
+    backgrounds=[img,np.full([height,width,3],(51,47,44),dtype=np.uint8), (frame if (frame is not None) else np.full([height,width,3],(0,0,0),dtype=np.uint8))]
+
 
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
@@ -45,76 +56,86 @@ while(True):
     # hsat = cv.getTrackbarPos('hsat', 'settings')
     # hval = cv.getTrackbarPos('hval', 'settings')
 
-    lhue=95
-    lsat=197
-    lval=0 
-    hhue=102
-    hsat=255
-    hval=253
+    # Med Glove
+    # lhue = 95
+    # lsat = 197
+    # lval = 0
+    # hhue = 102
+    # hsat = 255
+    # hval = 253
+
+    lhue = 5
+    lsat = 102
+    lval = 195
+    hhue = 25
+    hsat = 190
+    hval = 255
 
     out = cv.inRange(hsv, np.array(
         [lhue, lsat, lval]), np.array([hhue, hsat, hval]))
 
-    # cv.imshow('m',out) 
+    # cv.imshow('m', out)
 
     # Find Center
     M = cv.moments(out)
-    cursor=None
+    cursor = None
     if M["m00"] != 0:
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
         if draw:
             if newLine:
-                newLine=False
+                newLine = False
                 lines.append([color])
             lines[-1].append((cX, cY))
         else:
-            cursor=(cX,cY)
+            cursor = (cX, cY)
 
-    img=np.full([height,width,3],(51,47,44),dtype=np.uint8)
+    # img=np.full([height,width,3],(51,47,44),dtype=np.uint8)
+    img=backgrounds[curBG]
+    unedited=frame
+
 
     # Plot drawings
     for cur in lines:
-        points=cur[1:]
-        thisColor=cur[0]
+        points = cur[1:]
+        thisColor = cur[0]
         for i in range(1, len(points)):
             if(type(points[i]) == tuple and type(points[i-1]) == tuple):
                 cv.line(img, points[i], points[i-1], thisColor, 4)
 
     # Plot Buttons
     if not draw:
-        num=len(circles.circles)
-        radius=round(((height/num)-(pad*2))/2)
+        num = len(circles.circles)
+        radius = round(((height/num)-(pad*2))/2)
 
         # Plot each button
-        for i in range(0,num):
-            current=circles.circles[i]
-            
-            # Find coordinates for each button
-            x=round(radius+pad)
-            y=round((((height/num)*i) + ((height/num)/2)))
+        for i in range(0, num):
+            current = circles.circles[i]
 
-            # If button is a color swatch...
-            if current.color:
-                cv.circle(img, (x,y),radius,current.color,-1)
+            # Find coordinates for the button
+            x = round(radius+pad)
+            y = round((((height/num)*i) + ((height/num)/2)))
+
+            # Draw
+            current.draw(img, x, y, radius)
 
             if(cursor):
-                d=np.sqrt(((cursor[0]-x)**2)+((cursor[1]-y)**2))
-                if(d<=radius):
+                d = np.sqrt(((cursor[0]-x)**2)+((cursor[1]-y)**2))
+                if(d <= radius):
 
                     # If button is a color swatch...
                     if current.color:
-                        color=current.color
+                        color = current.color
 
     # Plots cursor when not drawing.
     if(cursor):
-        cv.circle(img,cursor,5,(0,0,0),-1)
-        cv.circle(img,cursor,3,color,-1)
-        
-
+        cv.circle(img, cursor, 5, (0, 0, 0), -1)
+        cv.circle(img, cursor, 3, color, -1)
 
     cv.imshow('PSPaint', img)
 
+    # if(curBG==2):
+    #     frame=unedited
 
     # UX
     val = cv.waitKey(1)
@@ -123,8 +144,17 @@ while(True):
         lines = []
     if(val == ord(' ')):
         draw = not draw
-        newLine=True
-    if(val ==ord('c')):
-        del lines[-1]
+        newLine = True
+    if(val == ord('c')):
+        if not draw:
+            del lines[-1]
+    if(val == ord('b')):
+        if(curBG<(len(backgrounds)-1)):
+            curBG=curBG+1
+        else:
+            curBG=0
+    if(val==ord('f')):
+        orig=cv.flip(orig,1)
+        cv.imwrite('assets/cur.jpg',orig)
     if(val == ord('q')):
         break

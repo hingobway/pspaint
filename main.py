da7066d153f99b6d3004767ce525a9268a7954e1
hingobway <hingobway@users.noreply.github.com>
2020-01-16 22:48:52 -0500

    last commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import circles
 import cv2 as cv
 import numpy as np
-import time 
+from matplotlib import pyplot as plt
+
+import time
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -9,12 +12,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-import circles
 
 cloudinary.config(
-    cloud_name = 'pspaint',  
-    api_key = os.getenv('CKEY'),  
-    api_secret = os.getenv('CSECRET')
+    cloud_name='pspaint',
+    api_key=os.getenv('CKEY'),
+    api_secret=os.getenv('CSECRET')
 )
 
 cap = cv.VideoCapture(0)
@@ -23,17 +25,32 @@ cap = cv.VideoCapture(0)
 def die(*arg):
     pass
 
+
+def plotLink(img, id):
+    link = 'pspaint.xyz/i/' + id
+    pos = (165, 450)
+    size = 1.5
+    thick = 4
+    cv.putText(img, link, pos, cv.FONT_HERSHEY_SIMPLEX,
+               size, (0, 0, 0), (thick+2), cv.LINE_AA)
+    cv.putText(img, link, pos, cv.FONT_HERSHEY_SIMPLEX,
+               size, (255, 255, 255), thick, cv.LINE_AA)
+
+
 def saveImage(img):
-    ts=str(int(time.time()))
-    fname="assets/picture" + ts + '.jpg'
-    cv.imwrite(fname,img)
-    resp=cloudinary.uploader.upload(fname)
-    url=resp['secure_url']
-    r = requests.post('https://pspaint.xyz/api/upload', data = {'url':url, 'timestamp':ts})
+    ts = str(int(time.time()))
+    fname = "assets/picture" + ts + '.jpg'
+    cv.imwrite(fname, img)
+    resp = cloudinary.uploader.upload(fname)
+    url = resp['secure_url']
+    r = requests.post('https://pspaint.xyz/api/upload',
+                      data={'url': url, 'timestamp': ts})
+    # r = requests.post('http://localhost:5000/api/upload', data = {'url':url, 'timestamp':ts})
     if(r.status_code is not 200):
         # freak out
         pass
     os.remove(fname)
+    return r.json()['snow']
 
 
 # cv.namedWindow('settings')
@@ -47,30 +64,31 @@ def saveImage(img):
 
 lines = []
 draw = False
-save=False
+save = False
 newLine = False
+oldImg = None
 
 color = (255, 255, 255)
 pad = 15
-curBG=0
+curBG = 0
 
-frame=None
+frame = None
 
 while(True):
     _, img = cap.read()
-    orig=img
+    orig = img
     # img = cv.resize(img, (1920,1010), interpolation=cv.INTER_LINEAR)
     height, width = img.shape[:2]
 
     img = cv.flip(img, 1)
-    
+
     try:
-        frame=cv.imread('assets/cur.jpg')
+        frame = cv.imread('assets/cur.jpg')
     except:
         pass
 
-    backgrounds=[img,np.full([height,width,3],(51,47,44),dtype=np.uint8), (frame if (frame is not None) else np.full([height,width,3],(0,0,0),dtype=np.uint8))]
-
+    backgrounds = [img, np.full([height, width, 3], (51, 47, 44), dtype=np.uint8), (frame if (
+        frame is not None) else np.full([height, width, 3], (0, 0, 0), dtype=np.uint8))]
 
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
@@ -83,7 +101,7 @@ while(True):
     # hsat = cv.getTrackbarPos('hsat', 'settings')
     # hval = cv.getTrackbarPos('hval', 'settings')
 
-    # Med Glove
+    # # Med Glove
     # lhue = 95
     # lsat = 197
     # lval = 0
@@ -92,20 +110,28 @@ while(True):
     # hval = 253
 
     # Orange Finger
-    # lhue = 5
-    # lsat = 102
-    # lval = 195
-    # hhue = 25
-    # hsat = 190
+    lhue = 5
+    lsat = 102
+    lval = 195
+    hhue = 25
+    hsat = 190
+    hval = 255
+
+    # # Phone Blob
+    # lhue = 98
+    # lsat = 91
+    # lval = 215
+    # hhue = 107
+    # hsat = 162
     # hval = 255
 
-    # Phone Blob
-    lhue = 98
-    lsat = 91
-    lval = 215
-    hhue = 107
-    hsat = 162
-    hval = 255
+    # # Phone Blob 2
+    # lhue = 86
+    # lsat = 86
+    # lval = 179
+    # hhue = 104
+    # hsat = 157
+    # hval = 255
 
     out = cv.inRange(hsv, np.array(
         [lhue, lsat, lval]), np.array([hhue, hsat, hval]))
@@ -128,9 +154,8 @@ while(True):
             cursor = (cX, cY)
 
     # img=np.full([height,width,3],(51,47,44),dtype=np.uint8)
-    img=backgrounds[curBG]
-    unedited=frame
-
+    img = backgrounds[curBG]
+    unedited = frame
 
     # Plot drawings
     for cur in lines:
@@ -142,10 +167,13 @@ while(True):
 
     # Plot Buttons
     if not draw and not save:
-        num = len(circles.circles)
-        radius = round(((height/num)-(pad*2))/2)
+        # Plot the last image link
+        if(oldImg is not None):
+            plotLink(img, oldImg)
 
         # Plot each button
+        num = len(circles.circles)
+        radius = round(((height/num)-(pad*2))/2)
         for i in range(0, num):
             current = circles.circles[i]
 
@@ -172,33 +200,34 @@ while(True):
     cv.imshow('PSPaint', img)
 
     if(save):
-        save=False
-        saveImage(img)
+        save = False
+        oldImg = saveImage(img)
 
     # UX
     val = cv.waitKey(1)
 
-    if(val == ord('m')):
+    if(val == ord('q')):
         lines = []
-    if(val == ord(' ')):
+    if(val == ord('d')):
+        if(oldImg is not None):
+            oldImg = None
         draw = not draw
         newLine = True
-    if(val == ord('c')):
+    if(val == ord('v')):
         if not draw:
             del lines[-1]
-    if(val == ord('b')):
-        if(curBG<(len(backgrounds)-1)):
-            curBG=curBG+1
+    if(val == ord('e')):
+        if(curBG < (len(backgrounds)-1)):
+            curBG = curBG+1
         else:
-            curBG=0
-    if(val==ord('f')):
-        orig=cv.flip(orig,1)
-        cv.imwrite('assets/cur.jpg',orig)
-        curBG=2
-    if(val==ord('s')):
+            curBG = 0
+    if(val == ord('r')):
+        orig = cv.flip(orig, 1)
+        cv.imwrite('assets/cur.jpg', orig)
+        curBG = 2
+    if(val == ord('a')):
+        draw = False
         if(not save):
-            save=True
-    if(val == ord('q')):
+            save = True
+    if(val == ord('z') or val == ord('n')):
         break
-
-
